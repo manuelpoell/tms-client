8b6cc0202eb0b3507fd01aab8c6b5118db902fa0
Manuel Poell <manuelpoell@gmx.at>
2022-12-13 22:52:11 +0100

    install angular material

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,8 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { TMSComponent } from './app/tms.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { importProvidersFrom } from '@angular/core';
 
-bootstrapApplication(TMSComponent);
+bootstrapApplication(TMSComponent, {
+    providers: [importProvidersFrom(BrowserAnimationsModule)]
+});
